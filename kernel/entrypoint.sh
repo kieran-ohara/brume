ee8e67df6c5e295bd397e8133ec015d5ebb2a899
Kieran Bamforth <hello@kieranbamforth.me>
2021-12-16 12:25:58 +0000

    Build the modules for the Marvell kernel.
    The virtual machines are not able to execute ip-tables-related tasks as
    the kernel module does not exist. Whilst it is unlikely that this commit
    will fix _that_ issue, I note that the current build tasks do not build
    the modules, therefore this is probably a good first step.

--- kernel/entrypoint.sh
+++ kernel/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
-make mvebu_v8_lsp_defconfig
+# I may not want this for non-brume builds.
+make mvebu_v8_lsp_defconfig || true
 # I seem to have to curb the amount of parallel processes or I get "transport endpoint is not connected".
 make -j${MAKE_PARALLEL:-4}
+make -j${MAKE_PARALLEL:-4} modules
+make -j${MAKE_PARALLEL:-4} modules_install INSTALL_MOD_PATH=arch/arm64/modules
